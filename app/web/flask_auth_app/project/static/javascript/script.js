3a09f3bd0ee864c23c782d8e7a29ce7205587ec7
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-05-19 16:14:43 +0300

    Update script.js

--- app/web/flask_auth_app/project/static/javascript/script.js
+++ app/web/flask_auth_app/project/static/javascript/script.js
@@ -1,3 +1,4 @@
+import showdown from 'showdown'; // Import Showdown library
 import { renderLatestMarkdown } from './block.js';
 
 // Fetch JSON data from the server and render markdown content
@@ -5,6 +6,14 @@ fetch('markdown_output.json')
     .then(response => response.json())
     .then(data => {
         console.log('Fetched data:', data); // Debugging: Log fetched data
+
+        // Create a new instance of Showdown converter
+        const converter = new showdown.Converter();
+
+        // Convert Markdown content to HTML
+        data.forEach(item => {
+            item.htmlContent = converter.makeHtml(item.content);
+        });
 
         // Call the renderLatestMarkdown function with the fetched data
         renderLatestMarkdown(data);
