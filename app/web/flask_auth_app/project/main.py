0ebf17967a6408054e1a86c27bd946dc1803eb92
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-05-19 19:35:51 +0300

    Update main.py

--- app/web/flask_auth_app/project/main.py
+++ app/web/flask_auth_app/project/main.py
@@ -9,7 +9,6 @@ main_blueprint = Blueprint('main', __name__)
 @main_blueprint.route('/')
 def index():
     # Construct the file path relative to the application root
-    #json_file_path = os.path.join(current_app.root_path, 'markdown_output.json')
     json_file_path = '/web/flask_auth_app/markdown_output.json'
     
     # Read data from markdown_output.json
@@ -17,12 +16,20 @@ def index():
         data = json.load(json_file)
     
     # Filter data for entries where trending, topPick, or popular is "on"
-    filtered_data = [
-        item for item in data
-        if item['metadata'].get('trending') == 'on' or
-           item['metadata'].get('topPick') == 'on' or
-           item['metadata'].get('popular') == 'on'
-    ]
+    filtered_data = []
+    trending_count = 0
+    top_pick_count = 0
+    popular_count = 0
+    for item in data:
+        if trending_count < 1 and item['metadata'].get('trending') == 'on':
+            filtered_data.append(item)
+            trending_count += 1
+        elif top_pick_count < 1 and item['metadata'].get('topPick') == 'on':
+            filtered_data.append(item)
+            top_pick_count += 1
+        elif popular_count < 2 and item['metadata'].get('popular') == 'on':
+            filtered_data.append(item)
+            popular_count += 1
     
     # Pass the filtered data to the template for rendering
     return render_template('index.html', data=filtered_data)
