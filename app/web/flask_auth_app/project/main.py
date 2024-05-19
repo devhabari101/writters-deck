27810b0cfdf5ecc03fe246f13520eb1a8dd89039
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-05-19 19:09:04 +0300

    Update main.py

--- app/web/flask_auth_app/project/main.py
+++ app/web/flask_auth_app/project/main.py
@@ -9,17 +9,23 @@ main_blueprint = Blueprint('main', __name__)
 @main_blueprint.route('/')
 def index():
     # Construct the file path relative to the application root
+    #json_file_path = os.path.join(current_app.root_path, 'markdown_output.json')
     json_file_path = '/web/flask_auth_app/markdown_output.json'
     
     # Read data from markdown_output.json
     with open(json_file_path, 'r') as json_file:
         data = json.load(json_file)
     
-    # Filter the data to get the latest trending, topPick, and popular markdown
-    latest_markdown = next((item for item in data if item['metadata'].get('trending') == 'on' and item['metadata'].get('topPick') == 'on' and item['metadata'].get('popular') == 'on'), None)
+    # Filter data for entries where trending, topPick, or popular is "on"
+    filtered_data = [
+        item for item in data
+        if item['metadata'].get('trending') == 'on' or
+           item['metadata'].get('topPick') == 'on' or
+           item['metadata'].get('popular') == 'on'
+    ]
     
-    # Pass data to the template for rendering
-    return render_template('index.html', latest_markdown=latest_markdown)
+    # Pass the filtered data to the template for rendering
+    return render_template('index.html', data=filtered_data)
 
 @main_blueprint.route('/profile')
 @login_required
