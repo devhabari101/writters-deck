5ec80e2a3d265c357ca6cc221e66dffb56171a7b
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-05-20 19:27:41 +0300

    Update trending_post.py

--- app/web/flask_auth_app/project/trending_post.py
+++ app/web/flask_auth_app/project/trending_post.py
@@ -4,9 +4,8 @@ import markdown
 from datetime import datetime
 from .convertor import markdown_dir  # Import markdown_dir
 
-def list_trending_post():
-    trending_post = None
-    latest_date = None
+def read_markdown_files():
+    posts = []
     for filename in os.listdir(markdown_dir):
         if filename.endswith(".md"):
             with open(os.path.join(markdown_dir, filename), "r", encoding="utf-8") as file:
@@ -20,17 +19,32 @@ def list_trending_post():
                     if len(key_value) == 2:
                         key, value = key_value
                         metadata_dict[key.strip()] = value.strip()
-                if metadata_dict.get('trending') == 'on':
-                    post_date_str = metadata_dict.get('date')
-                    if post_date_str:
-                        post_date = datetime.strptime(post_date_str, '%d-%m-%Y')
-                        if not latest_date or post_date > latest_date:
-                            latest_date = post_date
-                            html_content = markdown.markdown(content)
-                            # Remove <p> tags from the HTML content
-                            html_content = re.sub(r'<p>(.*?)</p>', r'\1', html_content)
-                            trending_post = {
-                                "metadata": metadata_dict,
-                                "content": html_content
-                            }
+                html_content = markdown.markdown(content)
+                # Remove <p> tags from the HTML content
+                html_content = re.sub(r'<p>(.*?)</p>', r'\1', html_content)
+                post = {
+                    "metadata": metadata_dict,
+                    "content": html_content
+                }
+                posts.append(post)
+    return posts
+
+def list_all_posts():
+    return read_markdown_files()
+
+def list_trending_post():
+    trending_post = None
+    latest_date = None
+    posts = read_markdown_files()
+
+    for post in posts:
+        metadata = post['metadata']
+        if metadata.get('trending') == 'on':
+            post_date_str = metadata.get('date')
+            if post_date_str:
+                post_date = datetime.strptime(post_date_str, '%d-%m-%Y')
+                if not latest_date or post_date > latest_date:
+                    latest_date = post_date
+                    trending_post = post
+
     return trending_post
