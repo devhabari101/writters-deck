ac2703459ef3bced18a84d88384cc99e7775ce45
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-05-24 09:42:25 +0300

    Update convertor.py

--- app/web/flask_auth_app/project/convertor.py
+++ app/web/flask_auth_app/project/convertor.py
@@ -143,3 +143,25 @@ def get_markdown_output():
     # Assuming markdown_output.json is in the root directory
     json_file_path = '/web/flask_auth_app/markdown_output.json'
     return send_file(json_file_path)
+
+
+# Route for detailed Markdown page
+@convertor_blueprint.route("/detail-page/<slug>")
+def detail_page(slug):
+    for filename in os.listdir(markdown_dir):
+        if filename.endswith(".md"):
+            with open(os.path.join(markdown_dir, filename), "r", encoding="utf-8") as file:
+                markdown_content = file.read()
+            metadata_match = re.match(r'^---(.*?)---(.*)', markdown_content, re.DOTALL)
+            if metadata_match:
+                metadata, content = metadata_match.groups()
+                metadata_dict = {}
+                for line in metadata.strip().split('\n'):
+                    key_value = line.split(':', 1)
+                    if len(key_value) == 2:
+                        key, value = key_value
+                        metadata_dict[key.strip()] = value.strip()
+                if metadata_dict.get('slug') == slug:
+                    html_content = markdown.markdown(content)
+                    return render_template("detail_page.html", metadata=metadata_dict, content=html_content)
+    abort(404)
