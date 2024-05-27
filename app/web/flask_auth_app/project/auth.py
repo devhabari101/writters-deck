c04f6e2e98168a33f182928690e161a573f2d525
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-05-27 16:02:26 +0300

    Update auth.py

--- app/web/flask_auth_app/project/auth.py
+++ app/web/flask_auth_app/project/auth.py
@@ -1,22 +1,20 @@
-# auth.py
-
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user, LoginManager
 from .models import User
 from . import db
+import os
+import re
 
 auth_blueprint = Blueprint('auth', __name__)
 login_manager = LoginManager()
 
 @login_manager.user_loader
 def load_user(user_id):
-    # Implement this function to load a user by ID
     return User.query.get(int(user_id))
 
 @login_manager.unauthorized_handler
 def unauthorized():
-    # Handle unauthorized access by redirecting users to the login page:
     return redirect(url_for('auth.login'))
 
 @auth_blueprint.route('/login')
@@ -24,7 +22,7 @@ def login():
     if current_user.is_authenticated:
         return redirect(url_for('main.index'))
     return render_template('login.html')
-    
+
 @auth_blueprint.route('/login', methods=['POST'])
 def login_post():
     email = request.form.get('email')
@@ -39,7 +37,7 @@ def login_post():
 
     login_user(user, remember=remember)
     return redirect(url_for('main.index'))
-    
+
 @auth_blueprint.route('/signup')
 def signup():
     if current_user.is_authenticated:
@@ -73,3 +71,34 @@ def signup_post():
 def logout():
     logout_user()
     return redirect(url_for('main.index'))
+
+# Directory containing Markdown files
+markdown_dir = "content"
+
+def list_user_markdowns():
+    user_id = current_user.id
+    user_markdowns = []
+
+    for filename in os.listdir(markdown_dir):
+        if filename.endswith(".md"):
+            with open(os.path.join(markdown_dir, filename), "r", encoding="utf-8") as file:
+                markdown_content = file.read()
+            metadata_match = re.match(r'^---(.*?)---(.*)', markdown_content, re.DOTALL)
+            if metadata_match:
+                metadata, _ = metadata_match.groups()
+                metadata_dict = {}
+                for line in metadata.strip().split('\n'):
+                    key_value = line.split(':', 1)
+                    if len(key_value) == 2:
+                        key, value = key_value
+                        metadata_dict[key.strip()] = value.strip()
+                if metadata_dict.get('user_id') == str(user_id):
+                    user_markdowns.append(metadata_dict)
+    
+    return user_markdowns
+
+@auth_blueprint.route("/admin", methods=["GET"])
+@login_required
+def form_admin():
+    user_markdowns = list_user_markdowns()
+    return render_template("admin/profile.html", name=current_user.name, email=current_user.email, user_markdowns=user_markdowns)
