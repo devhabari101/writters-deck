9c4a7a5c95bd7efec4a7353c8e7649cf5b7dae90
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-05-27 16:13:28 +0300

    Update main.py

--- app/web/flask_auth_app/project/main.py
+++ app/web/flask_auth_app/project/main.py
@@ -62,3 +62,25 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+
+# Route for detailed Markdown page
+@convertor_blueprint.route("/detail-page/<slug>")
+def detail_page(slug):
+    for filename in os.listdir(markdown_dir):
+        if filename.endswith(".md"):
+            with open(os.path.join(markdown_dir, filename), "r", encoding="utf-8") as file:
+                markdown_content = file.read()
+            metadata_match = re.match(r'^---(.*?)---(.*)', markdown_content, re.DOTALL)
+            if metadata_match:
+                metadata, content = metadata_match.groups()
+                metadata_dict = {}
+                for line in metadata.strip().split('\n'):
+                    key_value = line.split(':', 1)
+                    if len(key_value) == 2:
+                        key, value = key_value
+                        metadata_dict[key.strip()] = value.strip()
+                if metadata_dict.get('slug') == slug:
+                    html_content = markdown.markdown(content)
+                    return render_template("detail_page.html", metadata=metadata_dict, content=html_content)
+    abort(404)
