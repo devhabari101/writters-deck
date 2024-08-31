1c2fd1daf741474e02d2dafc99bd487a0b8d02c5
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-08-31 14:09:26 +0300

    Update main.py

--- app/web/flask_auth_app/project/main.py
+++ app/web/flask_auth_app/project/main.py
@@ -1,5 +1,5 @@
 import json
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, current_user
 from .auth import auth_blueprint  # Import the auth blueprint
 
@@ -12,6 +12,15 @@ def index():
 @main_blueprint.route('/archive.html')
 def archive():
     return render_template('archive.html')
+    
+@main_blueprint.route('/post-detail.html')
+def post_detail():
+    slug = request.args.get('slug')
+    if not slug:
+        return "Slug not provided", 400
+
+    # Assuming you might want to pass the slug or other data to the template
+    return render_template('post-detail.html', slug=slug)
 
 
 @main_blueprint.route('/profile')
