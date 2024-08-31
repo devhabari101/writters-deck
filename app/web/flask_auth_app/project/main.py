48fd0250f91f966c38840cd95f654a84a9bf028b
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-08-31 15:18:33 +0300

    Update main.py

--- app/web/flask_auth_app/project/main.py
+++ app/web/flask_auth_app/project/main.py
@@ -5,6 +5,16 @@ from .auth import auth_blueprint  # Import the auth blueprint
 
 main_blueprint = Blueprint('main', __name__)
 
+# Function to get post data by slug
+def get_post_by_slug(slug):
+    with open('markdown_output.json', 'r', encoding='utf-8') as json_file:
+        posts = json.load(json_file)
+        # Loop through the posts to find the one with the matching slug
+        for post in posts:
+            if post['metadata'].get('slug') == slug:
+                return post
+    return None
+
 @main_blueprint.route('/')
 def index():
     return render_template('index.html')
@@ -12,14 +22,15 @@ def index():
 @main_blueprint.route('/archive.html')
 def archive():
     return render_template('archive.html')
-    
+
 @main_blueprint.route('/post-detail.html')
 def post_detail():
     slug = request.args.get('slug')
     # Load the content associated with the slug
     post_data = get_post_by_slug(slug)  # Implement this function based on your data storage
+    if post_data is None:
+        return "Post not found", 404  # Handle case where post is not found
     return render_template('post-detail.html', post=post_data)
-
 
 @main_blueprint.route('/profile')
 @login_required
