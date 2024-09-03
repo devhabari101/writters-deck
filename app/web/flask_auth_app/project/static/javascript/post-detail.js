9b8493fed92f0b63f439f6c3efc2244562136a14
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-09-03 16:32:05 +0300

    Update post-detail.js

--- app/web/flask_auth_app/project/static/javascript/post-detail.js
+++ app/web/flask_auth_app/project/static/javascript/post-detail.js
@@ -5,6 +5,9 @@ console.log('post-detail.js script loaded');
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
 
+    // Call createSidebar here
+    createSidebar();
+
     fetch('markdown_output.json')
         .then(response => response.json())
         .then(data => {
