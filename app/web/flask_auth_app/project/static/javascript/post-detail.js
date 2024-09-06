9c3d8264a1aaca59e2bd955c28e2cb4e960f1b3e
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-09-06 12:04:23 +0300

    Update post-detail.js

--- app/web/flask_auth_app/project/static/javascript/post-detail.js
+++ app/web/flask_auth_app/project/static/javascript/post-detail.js
@@ -1,33 +1,29 @@
 import { createSidebar } from './sidebar.js';
 
+// Function to extract query parameters from the URL
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+// Get the slug from the URL
+const slug = getQueryParam('slug');
+
 fetch('markdown_output.json')
     .then(response => response.json())
     .then(data => {
         try {
-            console.log('Fetched data:', data);
-
-            const parseDate = (dateString) => {
-                const dateParts = dateString.split('-');
-                if (dateParts.length === 3) {
-                    const [day, month, year] = dateParts;
-                    return new Date(`${year}-${month}-${day}`);
-                } else {
-                    console.error('Invalid date format:', dateString);
-                    return new Date(dateString);
-                }
-            };
-
-            data.sort((a, b) => {
-                const dateA = parseDate(a.metadata.date);
-                const dateB = parseDate(b.metadata.date);
-                return dateB - dateA;
-            });
-
-            console.log('Sorted data:', data);
+            // Find the post by slug
+            const post = data.find(item => item.metadata.slug === slug);
+            if (!post) {
+                console.error('Post not found for slug:', slug);
+                return;
+            }
 
             const markdownContentDiv = document.getElementById('main-content');
             const converter = new showdown.Converter();
 
+            // Create the section and container structure
             const section = document.createElement('section');
             section.classList.add('bg0', 'p-t-62', 'p-b-60');
 
@@ -47,129 +43,78 @@ fetch('markdown_output.json')
             columnDiv.appendChild(innerColumnDiv);
             rowDiv.appendChild(columnDiv);
 
-            const displayArchivePosts = (posts) => {
-                innerColumnDiv.innerHTML = '';
+            // Display detailed post
+            const itemBlogDiv = document.createElement('div');
+            itemBlogDiv.classList.add('p-b-63');
 
-                posts.forEach(post => {
-                    const itemBlogDiv = document.createElement('div');
-                    itemBlogDiv.classList.add('p-b-63');
+            const linkElement = document.createElement('a');
+            linkElement.href = `post-detail.html?slug=${post.metadata.slug}`;
+            linkElement.classList.add('hov-img0', 'how-pos5-parent');
 
-                    const linkElement = document.createElement('a');
-                    linkElement.href = `post-detail.html?slug=${post.metadata.slug}`;
-                    linkElement.classList.add('hov-img0', 'how-pos5-parent');
+            const imageElement = document.createElement('img');
+            imageElement.src = post.metadata.image_url;
+            imageElement.alt = 'IMG-BLOG';
 
-                    const imageElement = document.createElement('img');
-                    imageElement.src = post.metadata.image_url;
-                    imageElement.alt = 'IMG-BLOG';
+            const dateDiv = document.createElement('div');
+            dateDiv.classList.add('flex-col-c-m', 'size-123', 'bg9', 'how-pos5');
 
-                    const dateDiv = document.createElement('div');
-                    dateDiv.classList.add('flex-col-c-m', 'size-123', 'bg9', 'how-pos5');
+            const [day, month, year] = post.metadata.date.split('-');
+            const monthName = new Date(`${year}-${month}-${day}`).toLocaleString('default', { month: 'short' });
 
-                    const [day, month, year] = post.metadata.date.split('-');
-                    const monthName = new Date(`${year}-${month}-${day}`).toLocaleString('default', { month: 'short' });
+            const daySpan = document.createElement('span');
+            daySpan.classList.add('ltext-107', 'cl2', 'txt-center');
+            daySpan.textContent = day;
 
-                    const daySpan = document.createElement('span');
-                    daySpan.classList.add('ltext-107', 'cl2', 'txt-center');
-                    daySpan.textContent = day;
+            const monthYearSpan = document.createElement('span');
+            monthYearSpan.classList.add('stext-109', 'cl3', 'txt-center');
+            monthYearSpan.textContent = `${monthName} ${year}`;
 
-                    const monthYearSpan = document.createElement('span');
-                    monthYearSpan.classList.add('stext-109', 'cl3', 'txt-center');
-                    monthYearSpan.textContent = `${monthName} ${year}`;
+            dateDiv.appendChild(daySpan);
+            dateDiv.appendChild(monthYearSpan);
+            linkElement.appendChild(imageElement);
+            linkElement.appendChild(dateDiv);
 
-                    dateDiv.appendChild(daySpan);
-                    dateDiv.appendChild(monthYearSpan);
-                    linkElement.appendChild(imageElement);
-                    linkElement.appendChild(dateDiv);
+            const contentDiv = document.createElement('div');
+            contentDiv.classList.add('p-t-32');
 
-                    const contentDiv = document.createElement('div');
-                    contentDiv.classList.add('p-t-32');
+            const titleElement = document.createElement('h4');
+            titleElement.classList.add('p-b-15');
+            titleElement.textContent = post.metadata.title;
 
-                    const titleElement = document.createElement('h4');
-                    titleElement.classList.add('p-b-15');
+            const contentParagraph = document.createElement('p');
+            contentParagraph.classList.add('stext-117', 'cl6');
+            const htmlContent = converter.makeHtml(post.content);
+            contentParagraph.innerHTML = htmlContent;
 
-                    
+            contentDiv.appendChild(titleElement);
+            contentDiv.appendChild(contentParagraph);
 
-                    const contentParagraph = document.createElement('p');
-                    contentParagraph.classList.add('stext-117', 'cl6');
-                    const htmlContent = converter.makeHtml(post.content);
-                    contentParagraph.innerHTML = htmlContent;
+            const authorCategoryDiv = document.createElement('div');
+            authorCategoryDiv.classList.add('flex-w', 'flex-sb-m', 'p-t-18');
 
-                    contentDiv.appendChild(titleElement);
-                    contentDiv.appendChild(contentParagraph);
+            const authorSpan = document.createElement('span');
+            authorSpan.classList.add('flex-w', 'flex-m', 'stext-111', 'cl2', 'p-r-30', 'm-tb-10');
+            authorSpan.innerHTML = `<span class="cl4">By</span> Admin <span class="cl12 m-l-4 m-r-6">|</span>`;
 
-                    const authorCategoryDiv = document.createElement('div');
-                    authorCategoryDiv.classList.add('flex-w', 'flex-sb-m', 'p-t-18');
+            const categorySpan = document.createElement('span');
+            categorySpan.textContent = post.metadata.category;
+            categorySpan.classList.add('cl4');
 
-                    const authorSpan = document.createElement('span');
-                    authorSpan.classList.add('flex-w', 'flex-m', 'stext-111', 'cl2', 'p-r-30', 'm-tb-10');
-                    authorSpan.innerHTML = `<span class="cl4">By</span> Admin <span class="cl12 m-l-4 m-r-6">|</span>`;
+            const separatorSpan = document.createElement('span');
+            separatorSpan.classList.add('cl12', 'm-l-4', 'm-r-6');
+            separatorSpan.textContent = '|';
 
-                    const categorySpan = document.createElement('span');
-                    categorySpan.textContent = post.metadata.category;
-                    categorySpan.classList.add('cl4');
+            authorSpan.appendChild(categorySpan);
+            authorSpan.appendChild(separatorSpan);
 
-                    const separatorSpan = document.createElement('span');
-                    separatorSpan.classList.add('cl12', 'm-l-4', 'm-r-6');
-                    separatorSpan.textContent = '|';
+            authorCategoryDiv.appendChild(authorSpan);
 
-                    authorSpan.appendChild(categorySpan);
-                    authorSpan.appendChild(separatorSpan);
+            itemBlogDiv.appendChild(linkElement);
+            itemBlogDiv.appendChild(contentDiv);
+            itemBlogDiv.appendChild(authorCategoryDiv);
+            innerColumnDiv.appendChild(itemBlogDiv);
 
-                    
-                    authorCategoryDiv.appendChild(authorSpan);
-                    
-
-                    itemBlogDiv.appendChild(linkElement);
-                    itemBlogDiv.appendChild(contentDiv);
-                    itemBlogDiv.appendChild(authorCategoryDiv);
-                    innerColumnDiv.appendChild(itemBlogDiv);
-                });
-            };
-
-            const categories = data.map(item => item.metadata.category);
-            const uniqueCategories = [...new Set(categories)];
-
-            const archiveMap = new Map();
-            data.forEach(markdown => {
-                const [day, month, year] = markdown.metadata.date.split('-');
-                const key = `${year}-${month}`;
-                if (!archiveMap.has(key)) {
-                    archiveMap.set(key, []);
-                }
-                archiveMap.get(key).push(markdown);
-            });
-
-            const sortedArchiveKeys = Array.from(archiveMap.keys()).sort((a, b) => new Date(b) - new Date(a));
-            const archiveDiv = document.createElement('div');
-            archiveDiv.classList.add('p-t-65');
-
-            const archiveTitle = document.createElement('h4');
-            archiveTitle.classList.add('mtext-112', 'cl2', 'p-b-33');
-            archiveTitle.textContent = 'Archive';
-
-            const archiveList = document.createElement('ul');
-
-            sortedArchiveKeys.forEach(key => {
-                const [year, month] = key.split('-');
-                const monthName = new Date(year, month - 1).toLocaleString('default', { month: 'long' });
-                const posts = archiveMap.get(key);
-                const archiveItem = document.createElement('li');
-
-                const archiveLink = document.createElement('a');
-                archiveLink.href = `/archive.html?month=${month}&year=${year}`;
-                archiveLink.classList.add('dis-block', 'stext-115', 'cl6', 'hov-cl1', 'trans-04', 'p-tb-8', 'p-lr-4');
-                archiveLink.textContent = `${monthName} ${year} (${posts.length})`;
-
-               
-
-                archiveItem.appendChild(archiveLink);
-                archiveList.appendChild(archiveItem);
-            });
-
-            archiveDiv.appendChild(archiveTitle);
-            archiveDiv.appendChild(archiveList);
-			
-			 // Append the YouTube video if the link is present
+            // Append the YouTube video if the link is present
             if (post.metadata.youtube_link) {
                 const youtubeVideoContainer = document.createElement('div');
                 youtubeVideoContainer.classList.add('video-container');
@@ -183,29 +128,27 @@ fetch('markdown_output.json')
                 iframe.allowFullscreen = true;
 
                 youtubeVideoContainer.appendChild(iframe);
-                mainContainer.appendChild(youtubeVideoContainer);
+                innerColumnDiv.appendChild(youtubeVideoContainer);
             }
 
-            const sidebar = createSidebar(uniqueCategories, data.filter(item => item.metadata.popular === 'on'));
-            sidebar.appendChild(archiveDiv);
+            // Add sidebar for related posts
+            const categories = data.map(item => item.metadata.category);
+            const uniqueCategories = [...new Set(categories)];
 
+            const sidebar = createSidebar(uniqueCategories, data.filter(item => item.metadata.popular === 'on'));
             rowDiv.appendChild(sidebar);
             markdownContentDiv.appendChild(section);
 
-            if (sortedArchiveKeys.length > 0) {
-                displayArchivePosts(archiveMap.get(sortedArchiveKeys[0]));
-            }
-
         } catch (error) {
-            console.error('Error processing data:', error);
+            console.error('Error processing post detail:', error);
         }
     })
     .catch(error => {
-        console.error('Error fetching markdown data:', error);
+        console.error('Error fetching post data:', error);
     });
-	
-	// Helper function to extract YouTube ID from URL
-    function extractYouTubeID(url) {
+
+// Helper function to extract YouTube ID from URL
+function extractYouTubeID(url) {
     const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/)|youtu\.be\/)([^&?/]+)/;
     const match = url.match(regex);
     return match ? match[1] : '';
